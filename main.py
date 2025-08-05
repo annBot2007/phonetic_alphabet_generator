86a6802c9a6eab00c54bfad2d179d4b664edbdb1
annBot2007 <85276629+annBot2007@users.noreply.github.com>
2025-08-05 12:17:27 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
+print(nato_dict)
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+
+    try:
+        nato_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(nato_list)
+
+generate_phonetic()
